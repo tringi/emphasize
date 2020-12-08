206a185f587c2b54c954f7657068a26f7d44648b
Tringi <tringi@trimcore.cz>
2020-12-08 05:04:17 +0100

    added QueryDynamicMemoryMaximum function

---
+++ Windows/Windows_QueryDynamicMemoryMaximum.cpp
@@ -0,0 +1,62 @@
+#include "Windows_QueryDynamicMemoryMaximum.hpp"
+#include <perflib.h>
+
+// HvDmvcsGuid
+//  - "Hyper-V Dynamic Memory Integration Service"
+//
+static const GUID HvDmvcsGuid = { 0x66f19dff, 0xa4dd, 0x4802, { 0x8f, 0xbb, 0x29, 0xe6, 0xa5, 0x4a, 0xf9, 0xda } };
+
+DWORD Windows::QueryDynamicMemoryMaximum (std::size_t * value) {
+    DWORD error = ERROR_SUCCESS;
+    HANDLE query = NULL;
+
+    if ((error = PerfOpenQueryHandle (NULL, &query)) == ERROR_SUCCESS) {
+
+        PERF_COUNTER_IDENTIFIER counter = {};
+        counter.CounterSetGuid = HvDmvcsGuid;
+        counter.Size = sizeof counter;
+        counter.CounterId = 1; // ID 1 == "Maximum Memory, Mbytes"
+
+        if ((error = PerfAddCounters (query, &counter, sizeof counter)) == ERROR_SUCCESS) {
+
+            struct {
+                PERF_DATA_HEADER    header;
+                PERF_COUNTER_HEADER counter;
+                PERF_COUNTER_DATA   counterdata;
+                ULONGLONG           value;
+            } data = {};
+
+            DWORD n;
+            if ((error = PerfQueryCounterData (query, &data.header, sizeof data, &n)) == ERROR_SUCCESS) {
+
+                switch (data.counter.dwType) {
+                    default:
+                        error = ERROR_INVALID_DATA;
+                        break;
+                    case PERF_ERROR_RETURN:
+                        error = data.counter.dwStatus;
+                        break;
+
+                    case PERF_SINGLE_COUNTER:
+                        switch (data.header.dwNumCounters) {
+                            default:
+                                error = ERROR_MORE_DATA;
+                                break;
+                            case 0:
+                                error = ERROR_NO_DATA;
+                                break;
+                            
+                            case 1:
+                                if (n >= (sizeof data - sizeof (DWORD))) { // if published as single DWORD instead of ULONGLONG
+                                    if (value) {
+                                        *value = data.value * 1024 * 1024;
+                                    }
+                                }
+                        }
+                }
+            }
+        }
+        PerfCloseQueryHandle (query);
+    }
+    return error;
+}
---
+++ Windows/Windows_QueryDynamicMemoryMaximum.hpp
@@ -0,0 +1,28 @@
+#ifndef WINDOWS_QUERYDYNAMICMEMORYMAXIMUM_HPP
+#define WINDOWS_QUERYDYNAMICMEMORYMAXIMUM_HPP
+
+#include <windows.h>
+#include <cstddef>
+
+namespace Windows {
+
+    // Windows::QueryDynamicMemoryMaximum
+    //  - uses Vista+ perfromance counters API to retrieve the value
+    //     - "Hyper-V Dynamic Memory Integration Service" / "Maximum Memory, MBytes"
+    //     - the value is acquired from root partition through VMBus in Kernel mode by DMVSC.sys
+    //       driver, and published only as a performance counter and WMI value (slow to query)
+    //     - if Dynamic Memory is not enabled, the value equals to assigned RAM
+    //  - ON SUCCESS: writes dynamic memory maximum into 'result' (in bytes)
+    //     - 'result' can be nullptr if the actual value is not needed
+    //     - result value is always in multiple of 2 MBs
+    //        - Hyper-V Dynamic Memory has granularity of 2 MB
+    //        - raw value stored is in MBs
+    //  - returns: ERROR_SUCCESS on success, otherwise error code from the earliest failing API
+    //     - common errors:
+    //        - ERROR_NO_DATA - incompatible protocol or not a Virtual Machine, but driver is running
+    //        - ERROR_ACCESS_DENIED - returns Performance Counters API inside Nano Server container
+    //
+    DWORD QueryDynamicMemoryMaximum (std::size_t * result);
+}
+
+#endif
